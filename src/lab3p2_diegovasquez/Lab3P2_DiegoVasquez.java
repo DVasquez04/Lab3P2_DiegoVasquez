62c52973d3d8049efd95e77d4996f4d193f46a76
Diego <diegomartinvasquezfunez008@gmail.com>
2023-08-04 14:47:47 -0600

    Casi terminado el agregar auto

--- src/lab3p2_diegovasquez/Lab3P2_DiegoVasquez.java
+++ src/lab3p2_diegovasquez/Lab3P2_DiegoVasquez.java
@@ -4,8 +4,14 @@
  */
 package lab3p2_diegovasquez;
 
+import java.awt.Color;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
+import javax.swing.JColorChooser;
 
 /**
  *
@@ -16,7 +22,8 @@ public class Lab3P2_DiegoVasquez {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        DateFormat df = new SimpleDateFormat("yyyy");
         ArrayList <Vehiculo> vehiculos = new ArrayList();
         Scanner lea = new Scanner(System.in);
         boolean seguir = true;
@@ -34,7 +41,38 @@ public class Lab3P2_DiegoVasquez {
             int op = lea.nextInt();
             switch(op){
                 case 1:{
-                    //Automivil
+                    //Automovil
+                    lea = new Scanner(System.in);
+                    System.out.println("Ingrese la placa: ");
+                    String placa = lea.next().toUpperCase();
+                    boolean valid = true;
+                    while(valid){
+                        if(placa.startsWith("H")){
+                            if(ValidPlaca(placa)){
+                                valid = false;
+                            }else{
+                                System.out.println("Placa invalida, intentela de nuevo");
+                                placa = lea.next().toUpperCase();
+                            }//fin else
+                        }else{
+                            System.out.println("Placa invalida, intentela de nuevo");
+                            placa = lea.next().toUpperCase();
+                        }//fin else
+                    }//fin valid placa
+                    System.out.println("Ingrese la marca: ");
+                    String marca = lea.next();
+                    System.out.println("Ingrese el modelo: ");
+                    String modelo = lea.next();
+                    System.out.println("Ingrese su tipo: (Turismo, Camioneta, etc.)");
+                    String tipo = lea.next();
+                    Color color = JColorChooser.showDialog(null, "Ingrese el COlor", Color.WHITE);
+                    System.out.println("Ingrese el año: yyyy");
+                    String año = lea.next();
+                    Date Año = df.parse(año);
+                    System.out.println("Ingrese el tipo de combustible: (Diesel, Regular, Super)");
+                    String tipoComb = lea.next();
+                    System.out.println("Ingrese el numero de puertas: ");
+                    int numPuertas = lea.nextInt();
                     
                 }//fin case
                 break;
@@ -63,7 +101,7 @@ public class Lab3P2_DiegoVasquez {
                 }//fin case
                 break;
                 case 8:{
-                    
+                    seguir = false;
                 }//fin case
                 break;
                 default:{
@@ -73,5 +111,26 @@ public class Lab3P2_DiegoVasquez {
             }
         }//fin while
     }//fin main
+    
+    public static boolean ValidPlaca(String p){
+        //returns true si es placa valida, y false si es no valida xd
+        boolean valid = true;
+        int contD = 0;
+        int contC = 0;
+        for (int i = 0; i < p.length(); i++) {
+            int loc = p.charAt(i);
+            if(loc >= 65 && loc <= 90){
+                contC++;
+            }//fin aumento de chars
+            if(loc >= 48 && loc <= 57){
+                contD++;
+            }//fin aumento de digitos
+        }//fin for
+        if(contD < 4 || contC < 3){
+            valid = false;
+        }
+        return valid;
+                
+    }
     
 }//fin class
