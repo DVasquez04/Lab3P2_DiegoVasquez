cbce86fb41130b02fe511b9dd2dd8f1138b72be6
Diego <diegomartinvasquezfunez008@gmail.com>
2023-08-04 14:12:53 -0600

    Creado la base del main

--- src/lab3p2_diegovasquez/Autobus.java
+++ src/lab3p2_diegovasquez/Autobus.java
@@ -4,10 +4,104 @@
  */
 package lab3p2_diegovasquez;
 
+import java.awt.Color;
+import java.util.Date;
+
 /**
  *
  * @author Diego Vasquez
  */
-public class Autobus {
+public class Autobus extends Vehiculo{
+    private int CapPasajeros;
+    private int NumEjes;
+    private double Longitud;
+
+    public Autobus() {
+    }
+
+    public Autobus(int CapPasajeros, int NumEjes, double Longitud, String Placa, String Marca, String Modelo, String tipo, Color color, Date año) {
+        super(Placa, Marca, Modelo, tipo, color, año);
+        this.CapPasajeros = CapPasajeros;
+        this.NumEjes = NumEjes;
+        this.Longitud = Longitud;
+    }
+
+    public int getCapPasajeros() {
+        return CapPasajeros;
+    }
+
+    public void setCapPasajeros(int CapPasajeros) {
+        this.CapPasajeros = CapPasajeros;
+    }
+
+    public int getNumEjes() {
+        return NumEjes;
+    }
+
+    public void setNumEjes(int NumEjes) {
+        this.NumEjes = NumEjes;
+    }
+
+    public double getLongitud() {
+        return Longitud;
+    }
+
+    public void setLongitud(double Longitud) {
+        this.Longitud = Longitud;
+    }
+
+    public String getPlaca() {
+        return Placa;
+    }
+
+    public void setPlaca(String Placa) {
+        this.Placa = Placa;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String Marca) {
+        this.Marca = Marca;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String Modelo) {
+        this.Modelo = Modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Date getAño() {
+        return año;
+    }
+
+    public void setAño(Date año) {
+        this.año = año;
+    }
+
+    @Override
+    public String toString() {
+        return "Autobus{" + "CapPasajeros=" + CapPasajeros + ", NumEjes=" + NumEjes + ", Longitud=" + Longitud + '}';
+    }
+    
     
 }
--- src/lab3p2_diegovasquez/Lab3P2_DiegoVasquez.java
+++ src/lab3p2_diegovasquez/Lab3P2_DiegoVasquez.java
@@ -4,6 +4,9 @@
  */
 package lab3p2_diegovasquez;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Diego Vasquez
@@ -14,7 +17,61 @@ public class Lab3P2_DiegoVasquez {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-    }
+        ArrayList <Vehiculo> vehiculos = new ArrayList();
+        Scanner lea = new Scanner(System.in);
+        boolean seguir = true;
+        while (seguir){
+            System.out.println("""
+                               =MENU VEHICULOS=
+                               1.Agregar Automóvil
+                               2.Agregar Motocicleta
+                               3.Agregar Autobus
+                               4.Modificar Vehículo
+                               5.Eliminar Vehiculo
+                               6.Mostrar Vehículos
+                               7.Generar Boleta
+                               8.Salir""");
+            int op = lea.nextInt();
+            switch(op){
+                case 1:{
+                    //Automivil
+                    
+                }//fin case
+                break;
+                case 2:{
+                    
+                }//fin case
+                break;
+                case 3:{
+                    
+                }//fin case
+                break;
+                case 4:{
+                    
+                }//fin case
+                break;
+                case 5:{
+                    
+                }//fin case
+                break;
+                case 6:{
+                    
+                }//fin case
+                break;
+                case 7:{
+                    
+                }//fin case
+                break;
+                case 8:{
+                    
+                }//fin case
+                break;
+                default:{
+                    
+                }//fin default
+                break;
+            }
+        }//fin while
+    }//fin main
     
-}
+}//fin class
